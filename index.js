05d4aedef17cf692888416e1c78b45b8cce09b0b
rodgeve <rodrigo.geve@solace.com>
2019-05-14 21:37:06 -0400

    Changes to make project work with Greg's NippleJS controller

--- index.js
+++ index.js
@@ -3,23 +3,27 @@ factoryProps.profile = solace.SolclientFactoryProfiles.version10;
 solace.SolclientFactory.init(factoryProps);
 
 let session = solace.SolclientFactory.createSession({
-    url:      'hostname',
-    vpnName:  'vpnname',
+    url:      'wss://',
+    vpnName:  'msgvpn',
     userName: 'solace-cloud-client',
     password: 'password',
 });
 
 session.on(solace.SessionEventCode.UP_NOTICE, function (sessionEvent) {
     console.log('=== Successfully connected and ready to subscribe. ===');
-    session.subscribe(solace.SolclientFactory.createTopicDestination('car/move'), true, 'car/move', 10000);
+    session.subscribe(solace.SolclientFactory.createTopic('car/drive/*'), true, 'car/drive/*', 10000);
 });
 
 session.on(solace.SessionEventCode.MESSAGE, function (message) {
     console.log(`Received message: "${message.getBinaryAttachment()}", details:\n ${message.dump()}`);
     let payload = JSON.parse(message.getBinaryAttachment())
-    gameInstance.SendMessage('CarKit', 'setLeftMotorSpeed', payload['l']);
-    gameInstance.SendMessage('CarKit', 'setRightMotorSpeed', payload['r']);
-    gameInstance.SendMessage('CarKit', 'setDriveDuration', payload['d']);
+    let topicLevels = message.xi.gl.split('/');
+    if (topicLevels.length == 3) {
+      let vehicleId = topicLevels[2];
+      gameInstance.SendMessage(vehicleId, 'setLeftMotorSpeed', payload['l']);
+      gameInstance.SendMessage(vehicleId, 'setRightMotorSpeed', payload['r']);
+      gameInstance.SendMessage(vehicleId, 'setDriveDuration', payload['d']);
+    }
 });
 
 session.connect();
